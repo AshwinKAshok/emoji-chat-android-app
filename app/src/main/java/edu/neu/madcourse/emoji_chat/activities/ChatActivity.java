dae217defa4086225ce189cea5d14513c9eb588e
Ashwin K Ashok <Ashwin Ashok>
2021-03-21 05:11:34 -0400

    [Task update by:Ashiwn] Firebase basic text message transfer and render established

--- app/src/main/java/edu/neu/madcourse/emoji_chat/activities/ChatActivity.java
+++ app/src/main/java/edu/neu/madcourse/emoji_chat/activities/ChatActivity.java
@@ -1,9 +1,17 @@
 package edu.neu.madcourse.emoji_chat.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import edu.neu.madcourse.emoji_chat.R;
 
@@ -15,6 +23,10 @@ public class ChatActivity extends AppCompatActivity {
 
     String sender_name;
     String receiver_name;
+
+    FirebaseDatabase root_node;
+    DatabaseReference child_node_sender;
+    DatabaseReference child_node_receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +45,47 @@ public class ChatActivity extends AppCompatActivity {
             receiver_name = extras.getString("receiver_name");
         }
 
+        root_node = FirebaseDatabase.getInstance();
+        child_node_sender = root_node.getReference("users").child(sender_name);
+        child_node_receiver = root_node.getReference("users").child(receiver_name);
 
+        child_node_sender.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                sender_messages.setText(text);
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        child_node_receiver.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                receiver_messages.setText(text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        send_message_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        child_node_receiver.setValue(enter_text_message.getText().toString());
+                    }
+                }).start();
+
+            }
+        });
     }
 }
