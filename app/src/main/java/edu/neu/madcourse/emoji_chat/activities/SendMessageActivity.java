c04d37eb08a85488092fd582f37f22103f9c30db
Akash Sasikumar <33842912+AkashSasikumar@users.noreply.github.com>
2021-03-22 16:06:53 -0400

    Added onClickListener to the emoji's to identify selected emoji. Highlighted the selected emoji. - Akash, Ashwin, Naveen, Preet

--- app/src/main/java/edu/neu/madcourse/emoji_chat/activities/SendMessageActivity.java
+++ app/src/main/java/edu/neu/madcourse/emoji_chat/activities/SendMessageActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
